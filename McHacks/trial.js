936ce57e8e0b51ddcb6ece32762f10e9c358b023
Ke Yan <ke.yan@mail.mcgill.ca>
2022-01-22 15:18:38 -0500

    trying to get image from folder

--- McHacks/trial.js
+++ McHacks/trial.js
@@ -1,6 +1,17 @@
-var rootref = firebase.database().ref().child("Images");
-rootref.on("child_added", snap => {
-    var image=snap.child("img").val;
 
-    $("#gallery_div").append("<img src=" + image + "></img>");
-})
+    console.log("hello");
+    var dir = "images/";
+    var fileextension = ".png";
+  $.ajax({
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: dir,
+    success: function (data) {
+      console.log("hello");
+        //List all .png file names in the page
+        $(data).find("a:contains(" + fileextension + ")").each(function () {
+            var filename = this.href.replace(window.location.host, "").replace("http://", "");
+            console.log("hello");
+            $("body").append("<img src='" + dir + filename + "'>");
+        });
+    }
+  });
