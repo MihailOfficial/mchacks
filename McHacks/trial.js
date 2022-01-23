e3192335431ef25e3810f2ba639c02c0b851a2db
J03YL1u <lucas.liuliongwah@mail.mcgill.ca>
2022-01-22 21:48:04 -0500

    added action to card

--- McHacks/trial.js
+++ McHacks/trial.js
@@ -1,6 +1,5 @@
 window.onload = function displayList() {
-
-  const prompt =[
+  const prompt = [
     "drink a liquid",
     "find something yellow",
     "jump!",
@@ -11,8 +10,8 @@ window.onload = function displayList() {
     "get a snack",
     "find a book",
     "find the first letter of your name",
-    "find a pet/plant"
-];
+    "find a pet/plant",
+  ];
 
   let entries = JSON.parse(localStorage.getItem("saved-sticker"));
   let html = "";
@@ -20,15 +19,13 @@ window.onload = function displayList() {
   for (i = 0; i < 9; i++) {
     const src = "/images/" + num;
     html +=
-        '<div class="flip-card"><div class="flip-card-inner"><div class="polaroid"> <a href="#" title="'+ prompt[num] +'"><img src="' +
-        src +
-        '.png" class="memories" title="Santorini"/></a></div><div class="flip-card-back"><h1>John Doe</h1><p>Architect & Engineer</p><p>We love that guy</p></div></div></div>';
+      '<div class="flip-card"><div class="flip-card-inner"><div class="polaroid"> <a href="#"><img src="' +
+      src +
+      '.png" class="memories" title="Santorini"/></a></div><div class="flip-card-back"><h1>" ' +
+      prompt[num] +
+      ' "</h1></div></div></div>';
 
     num += 1;
   }
   document.getElementById("guestview").innerHTML = html;
 };
-
-
-    
- 
