a4bae4bf16dbd762228a0639e7c3a0e47e2ef48f
Ke Yan <ke.yan@mail.mcgill.ca>
2022-01-22 21:25:12 -0500

    added promt in image

--- McHacks/trial.js
+++ McHacks/trial.js
@@ -1,11 +1,26 @@
 window.onload = function displayList() {
+
+  const prompt =[
+    "drink a liquid",
+    "find something yellow",
+    "jump!",
+    "make a heart",
+    "draw a cat",
+    "find a blue pen",
+    "put on some lip balm",
+    "get a snack",
+    "find a book",
+    "find the first letter of your name",
+    "find a pet/plant"
+];
+
   let entries = JSON.parse(localStorage.getItem("saved-sticker"));
   let html = "";
   var num = 0;
   for (i = 0; i < 9; i++) {
     const src = "/images/" + num;
     html +=
-        '<div class="polaroid"> <a href="#" title="Santorini"><img src="' +
+        '<div class="polaroid"> <a href="#" title="'+ prompt[num] +'"><img src="' +
         src +
         '.png" class="memories" title="Santorini"/></a></div>';
 
